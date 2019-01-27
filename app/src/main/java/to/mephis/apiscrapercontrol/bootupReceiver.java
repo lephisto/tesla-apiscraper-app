3225b1e0933c8bfafddc0a506ef636c1aed342ec
Bastian Maeuser <mephisto@mephis.to>
2019-01-27 23:03:42 +0100

    Autostart on Boot

---
+++ app/src/main/java/to/mephis/apiscrapercontrol/bootupReceiver.java
@@ -0,0 +1,17 @@
+package to.mephis.apiscrapercontrol;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class bootupReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Intent i = new Intent(context, ScraperActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+
+}
